b437b6970a546d8791dddc863615df8260901af7
Arvid Hammarlund <arvid.hammarlund@gmail.com>
2023-12-22 19:25:44 +0100

    Added data struct for init functionality

--- src/lib.rs
+++ src/lib.rs
@@ -3,34 +3,65 @@ use std::{fmt::Debug, hash::Hash};
 
 /// Unique identifier for a node
 ///
-pub trait Address {}
+pub trait Address: Clone + Debug + Eq + PartialEq + Hash {}
 
 impl Address for String {}
 impl Address for &str {}
 
-pub trait Body {}
+/// Internaly unique identifier for processed messages
+///
+pub trait MessageIndex: Clone + Debug + Eq + PartialEq + Hash + Ord + PartialOrd {}
 
-pub struct Node {}
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize, Hash)]
+pub enum InitType {
+    #[serde(rename = "init_ok")]
+    Request,
+    #[serde(rename = "init")]
+    Response,
+}
 
-/// Main communication medium for network
+/// This trait has to be implement for every Node alongside any workload specific functionality
+///
+pub trait InitHandler<A: Address, I: MessageIndex> {
+    fn respond_init(&self, incoming: InitRequest<A, I>) -> Result<InitRespond<I>, ()> {
+        Ok(InitRespond {
+            kind: incoming.kind,
+            in_reply_to: incoming.message_id,
+        })
+    }
+}
+
+/// Main communication medium for the network
 ///
 #[derive(Clone, Debug, PartialEq, Eq, Hash, Deserialize, Serialize, Default)]
-struct Message<A: Address, B: Body> {
+struct Message<A: Address, B> {
     #[serde(rename = "src")]
     pub source: A,
-    #[serde(rename = "src")]
+    #[serde(rename = "dst")]
     pub destination: A,
-    #[serde(rename = "src")]
     pub body: B,
 }
 
-/// Init message body
+/// Init message request message
 ///
-#[derive(Clone, Debug, PartialEq, Eq, Hash, Deserialize, Serialize, Default)]
-struct InitRequest<A: Address> {
+#[derive(Clone, Debug, PartialEq, Eq, Hash, Deserialize, Serialize)]
+pub struct InitRequest<A: Address, I: MessageIndex> {
     #[serde(rename = "type")]
-    kind: String,
+    pub kind: InitType,
     #[serde(rename = "msg_id")]
-    message_id: A,
-    #[serde()]
+    pub message_id: I,
+    pub node_id: A,
+    pub node_ids: Vec<A>,
 }
+
+/// Init message respond message
+///
+#[derive(Clone, Debug, PartialEq, Eq, Hash, Deserialize, Serialize)]
+pub struct InitRespond<I> {
+    #[serde(rename = "type")]
+    pub kind: InitType,
+    pub in_reply_to: I,
+}
+
+#[cfg(test)]
+mod test {}
